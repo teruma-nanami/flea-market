ecaf66bae3bc8f635adfc9d5d8cbe647644de6ca
teruma-nanami <kinos.travel.fun@gmail.com>
2024-09-22 18:10:56 +0900

    make navi and login register

--- src/resources/views/auth/forgot-password.blade.php
+++ src/resources/views/auth/forgot-password.blade.php
@@ -2,14 +2,13 @@
 
 @section('content')
   <div class="container">
-    <h2>Password Reset</h2>
+    <h2>パスワード再設定</h2>
     <form method="POST" action="{{ route('password.email') }}" class="form">
       @csrf
       <div class="form__inner">
         <p>パスワードを忘れてしまった場合、メールアドレスを入力してください。</p>
         <div class="form__inner-text">
-          <i class="bi bi-envelope-fill"></i>
-          <label for="email"></label>
+          <label for="email">メールアドレス</label>
           <input id="email" type="email" name="email" placeholder="email" required autofocus>
         </div>
         <div class="form__button">
